3d218bee003d0849ae2e1b08a677eb52f8a24406
Andrey Povaliy <116582306+AndreyPovaliy@users.noreply.github.com>
2023-03-03 19:51:40 +0300

    Task of lesson No. 2 are fixed (2)

--- src/JavaBasics/Lesson_2/Calculator/Calculator.java
+++ src/JavaBasics/Lesson_2/Calculator/Calculator.java
@@ -3,69 +3,69 @@ package JavaBasics.Lesson_2.Calculator;
 
 public class Calculator {
 
-    public static void  amount(Integer a, Integer b) {
+    public static void amount(Integer a, Integer b) {
         int r = a + b;
         System.out.println(r);
 
     }
 
-    public static void  amount(Double a, Double b) {
+    public static void amount(Double a, Double b) {
         double r = a + b;
         System.out.println(r);
 
     }
 
-    public static void  amount(Long a, Long b) {
+    public static void amount(Long a, Long b) {
         long r = a * b;
         System.out.println(r);
 
     }
 
-    public static void  subtraction(Integer a, Integer b) {
+    public static void subtraction(Integer a, Integer b) {
         int r = a - b;
         System.out.println(r);
 
     }
 
-    public static void  subtraction(Double a, Double b) {
+    public static void subtraction(Double a, Double b) {
         double r = a - b;
         System.out.println(r);
 
     }
 
-    public static void  subtraction(Long a, Long b) {
+    public static void subtraction(Long a, Long b) {
         long r = a - b;
         System.out.println(r);
 
     }
 
-    public static void  multiplication(Integer a, Integer b) {
+    public static void multiplication(Integer a, Integer b) {
         int r = a * b;
         System.out.println(r);
 
     }
 
-    public static void  multiplication(Double a, Double b) {
+    public static void multiplication(Double a, Double b) {
         double r = a * b;
         System.out.println(r);
     }
 
-    public static void  multiplication(Long a, Long b) {
+    public static void multiplication(Long a, Long b) {
         long r = a * b;
         System.out.println(r);
     }
 
-    public static void  division(int a, int b) {
+    public static void division(int a, int b) {
         double r = a / b;
         System.out.println(r);
     }
 
-    public static void  division(double a, double b) {
+    public static void division(double a, double b) {
         double r = a / b;
         System.out.println(r);
     }
 
-    public static void  division(long a, long b) {
+    public static void division(long a, long b) {
         double r = a / b;
         System.out.println(r);
     }
