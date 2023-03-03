4fb066461fc7c20a6fe1581c386dd07f4ac050ae
Andrey Povaliy <116582306+AndreyPovaliy@users.noreply.github.com>
2023-03-03 17:11:11 +0300

    Task of lesson No. 2 are fixed

--- src/JavaBasics/Lesson_2/Calculator/Calculator.java
+++ src/JavaBasics/Lesson_2/Calculator/Calculator.java
@@ -8,7 +8,27 @@ public class Calculator {
 
     }
 
+    public static double amount(double a, double b) {
+        return (a + b);
+
+    }
+
+    public static long amount(long a, long b) {
+        return (a + b);
+
+    }
+
     public static int subtraction(int a, int b) {
+        return (a - b);
+
+    }
+
+    public static double subtraction(double a, double b) {
+        return (a - b);
+
+    }
+
+    public static long subtraction(long a, long b) {
         return (a - b);
 
     }
@@ -18,7 +38,25 @@ public class Calculator {
 
     }
 
+    public static double multiplication(double a, double b) {
+        return (a * b);
+
+    }
+
+    public static long multiplication(long a, long b) {
+        return (a * b);
+
+    }
+
     public static int division(int a, int b) {
+        return (a / b);
+    }
+
+    public static double division(double a, double b) {
+        return (a / b);
+    }
+
+    public static long division(long a, long b) {
         return (a / b);
     }
 }
